fa72ab22141945e692b67de8ac5433459c4518ee
jayaramanrp <jayaramanrp04@gmail.com>
2020-04-25 12:41:11 +0530

    verison details

---
+++ py_pkg_version.py
@@ -0,0 +1,7 @@
+import sys
+import numpy as np
+import matplotlib
+
+print('Python:',sys.version) #3.8.2
+print('Numpy:', np.__version__) #1.18.3
+print('Matplotlib: ' , matplotlib.__version__) #3.2.1
